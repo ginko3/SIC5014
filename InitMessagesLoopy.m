4cc905d94e035fcb6aa0dbf6e978f0a35738979c
Max Cohen <max.cohen@gmx.com>
2019-01-30 17:16:51 +0100

    update files

---
+++ InitMessagesLoopy.m
@@ -0,0 +1,43 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Sum-product algorithm initialization  
+%
+% Assumption: size(node(i).CPT)=1 for all leaf nodes  
+%
+% Input arguments:
+% - N: number of nodes in the factor graph
+% - node[n]: node structure
+% - Adj[nxn]: adjacency matrix of the factor graph
+% - Message[nxn]: array of empty matrices representing the messages
+%
+% Output arguments:
+% - Message[nxn]: array of matrices representing the messages after
+%                   initialization
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function Message=InitMessagesLoopy(N,node,Adj,Message)
+
+% scan all nodes in the factor graph
+Message=cell(N,N);
+for X=1:N
+    % find all neighboring nodes of X
+    neighbors=find(Adj(X,:));
+    % init only leaf node messages
+    %if length(neighbors)==1
+        % init any variable nodes messages
+        if node(X).type=='variable'
+            % send  all one message to neighboring function nodes
+            for n=1:length(neighbors)
+                Message{X,neighbors(n)}=ones(size(node(X).values));
+            end
+        % init leaf function nodes messages   
+        elseif  node(X).type=='function' & length(neighbors)==1
+            % send function node to neighboring variable nodes
+            for n=1:length(neighbors)
+                Message{X,neighbors(n)}=node(X).CPT;
+            end
+        else
+            ;
+        end
+    %end
+end
---
+++ SumProductAlgorithmLoopy.m
@@ -0,0 +1,84 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Sum-product algorithm with flooding schedule: 
+% update until each edge in factor graph has been traversed in each direction  
+%
+% Input arguments:
+% - N: number of nodes in the factor graph
+% - node[N]: node structure
+% - Adj[NxN]: adjacency matrix of the factor graph
+% - Message[NxN]: array of empty matrices representing the messages
+%
+% Output arguments:
+% - Message[NxN]: array of matrices representing the messages after
+%                 the sum-product update
+% - Marginal[N]: array of matrices representing the marginals
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [Message,Marginal]=SumProductAlgorithmLoopy(N,node,Adj,Message)
+
+% scan all nodes while a message to a neighbor is empty 
+loop=1;
+while loop
+   for X=1:N
+        % find all neighboring nodes of X
+        neighbors=find(Adj(X,:));
+        % compute a message towards all neighbors of X - if possible
+        for n=1:length(neighbors)
+            % collect the set of neighbors\neighbor(n)
+            delta=setdiff(neighbors,neighbors(n));
+            % test if the message towards neighbors(n) is empty and computable
+            if isempty(Message{X,neighbors(n)}) && sum(cellfun(@isempty,Message(delta,X)))==0  
+                % update variable to function node messages
+                if node(X).type=='variable'
+                    % compute the product of all incoming messages from delta
+                    Message{X,neighbors(n)}=ones(size(node(X).values));
+                    for m=1:length(delta)
+                        Message{X,neighbors(n)}=Message{X,neighbors(n)}.*Message{delta(m),X};
+                    end
+                % update function to variable node messages 
+                elseif node(X).type=='function'
+                    % - compute function node x product of all incoming
+                    % messages from delta conserving the order of the variables
+                    w=1;
+                    for m=1:length(delta)
+                        w=kron(Message{delta(m),X},w);
+                    end
+                    % - and marginalize out all variables except neighbors(n)
+                    % for all values assumed by variable node neighbors(n)
+                    for v=1:length(node(neighbors(n)).values)
+                        % shift the dimension corresponding to neighbors(n) towards first dimension
+                        [B,I]=find(neighbors(n)==neighbors);
+                        prob=shiftdim(node(X).CPT,I-1);
+                        Message{X,neighbors(n)}(v)=sum(prob(v,:).*w);
+                    end
+                end
+            end
+        end
+   end
+    
+   % stopping rule: check if at least one message in the array is missing
+   loop=sum(cellfun(@isempty,Message(find(Adj))));
+end
+    
+% Termination: as soon as each edge in factor graph has been traversed in each direction
+% Collect set of variable nodes
+variable_nodes=[];
+for n=1:N
+    if(node(n).type=='variable')
+        variable_nodes=[variable_nodes,n];
+    end
+end
+
+% Compute marginal for each variable node
+for X=1:length(variable_nodes)
+     % find all neighboring nodes of X
+     neighbors=find(Adj(X,:));
+     % compute marginal of X
+     Marginal{X}=ones(size(node(X).values));
+     for m=1:length(neighbors)
+        Marginal{X}=Marginal{X}.*Message{neighbors(m),X};
+     end
+     % normalize marginal of X to probability mass function
+     Marginal{X}=Marginal{X}./sum(Marginal{X});
+end
